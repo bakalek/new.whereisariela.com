cb3ffe4458b9f6c0abeebce53f3f638178e7472b
bakalek <bakalek@users.noreply.github.com>
2013-10-06 01:42:43 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -34,16 +34,10 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>git deploy</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="background-color:#fff; color:#333;">
 <pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
 <?php echo $output; ?>
 </pre>
 </body>
